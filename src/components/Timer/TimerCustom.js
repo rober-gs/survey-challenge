d1712d3fa76fa8022cf55aaa6f47f8203ab01248
rober-gs <robertogarcia.dicio@gmail.com>
2022-02-28 14:26:11 -0600

    Add Timer For Surveys

---
+++ src/components/Timer/TimerCustom.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useTimer } from 'react-timer-hook';
+
+export const TimerCustom = ({timer, onExpire}) => {
+
+    const setTimestamp =(timer) => {
+        const timestamp = new Date();
+        return timestamp.setSeconds(timestamp.getSeconds() + timer);
+    }
+
+    const {
+        seconds,
+        minutes,
+        hours,        
+    } = useTimer({expiryTimestamp: setTimestamp(timer), onExpire: onExpire });
+        
+    const customFormat = display =>  {
+        return display.toString().length === 1 ? `0${display}` : display
+    }
+    
+    return (
+            <div style={{textAlign: 'center'}}>           
+            <div style={{fontSize: '40px'}}>
+               <span>{ customFormat(hours) }</span>
+               :<span>{ customFormat(minutes) }</span>
+               :<span>{ customFormat(seconds) }</span>
+            </div>
+            </div>
+        
+    )
+}
