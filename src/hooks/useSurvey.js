d6f1dd481c25a58cbf7cfdbc21ee7ec54fa3fd61
rober-gs <robertogarcia.dicio@gmail.com>
2022-02-28 14:29:54 -0600

    add custom hook for survey contract

--- src/hooks/useSurvey.js
+++ src/hooks/useSurvey.js
@@ -1,38 +1,105 @@
-import { useWeb3React } from '@web3-react/core';
+import Modal from 'antd/lib/modal/Modal';
 import { useCallback } from 'react';
 import { useAppContext } from '../AppContext';
+import { getSurveysData } from '../services/surveyServices';
+import { customFormat } from '../tools/utilities';
+
 
 export const useSurvey = () => {
-    
-    const { account } = useWeb3React();  
+
     const {
+        setSurveys,
         setBalance, 
-        setAvailable,
+        setReceipt,
+        setAvailable, 
+        setTransaction,
     } = useAppContext();
 
+    const getSurveys = () => {        
+        try  {
+            setSurveys( getSurveysData() );      
+        } catch( error ){
+            console.error("error", error);
+        }
+    }
 
-    const surveyAvailable = useCallback( async( contract ) => {
-
-        try {
+    const surveyAvailable = useCallback( async( contract, account ) => {
+        try {            
             
-            contract.surveyAvailable()
-                .then( available => setAvailable(available) );            
+            if (account) 
+            {
+                contract.surveyAvailable({
+                    from: account
+                })
+                .then( available => {                               
+                    setAvailable(available)
+                })
+                .catch( ({ message }) => {
+                    Modal.error({
+                        title: "Error",            
+                        content: message,    
+                        okText: "Try Again", 
+                        onOk: () => { window.location.reload() },
+                    });   
+                });          
+            }  
 
         } catch (error) {
-            console.log("error", error)
+            console.error("error", error)
         }
     },[]);
     
-    const getBalance = useCallback( async( contract ) => {
-       
-        try {
-            contract.balanceOf(account)
-                .then( balance => setBalance(balance) );            
-        
+    const getBalance = useCallback( async( contract, account ) => {
+        try {            
+            if(account)
+            {
+                contract.balanceOf(account, {
+                    from: account
+                })
+                .then( balance => {
+                    setBalance(customFormat(balance) )
+                }) 
+                .catch( ({ message }) => {
+                    Modal.error({
+                        title: "Error",            
+                        content: message,    
+                        okText: "Try Again", 
+                        onOk: () => { window.location.reload() },
+                    });   
+                });            
+            }
         } catch (error) {
-            console.log("error", error)
+            console.error("error", error)
         }
     },[]);
+
+    const addSurvey = useCallback(async(contract, account, response) => {
+
+        try {
+            if (account) 
+            {
+                console.log("addSurvey");
+                const responseStr = JSON.stringify(response)
+                console.log("respStr", responseStr)                
+                contract.addSurvey(responseStr)
+                .then( tx => {
+                    console.log("TX", tx);
+                    setTransaction( tx );
+                })
+                .catch( ({ message }) => {
+
+                    Modal.error({
+                        title: "MetaMask Error",            
+                        content: message,    
+                        okText: "Try Again", 
+                        onOk: () => { window.location.reload() },
+                    });   
+                });
+            }
+        } catch (error) {
+            console.error("error ", error)
+        }
+    }, []);
     
-    return{ surveyAvailable, getBalance }
+    return{ getSurveys, surveyAvailable, getBalance, addSurvey }    
 }
